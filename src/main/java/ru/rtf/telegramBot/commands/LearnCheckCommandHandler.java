fbfa98ceb9ac493e33a07fdebfca9504eaaf943b
Almazova Vlada <almazova.2004@mail.ru>
2024-11-13 21:11:17 +0500

    объединение команд режимов обучения в одну команду с разными параметрами и добавление режима "ввод термина"

---
+++ src/main/java/ru/rtf/telegramBot/commands/LearnCheckCommandHandler.java
@@ -0,0 +1,63 @@
+package ru.rtf.telegramBot.commands;
+
+import ru.rtf.Card;
+import ru.rtf.DeckManager;
+import ru.rtf.telegramBot.CommandHandler;
+import ru.rtf.telegramBot.learning.LearningSession;
+import ru.rtf.telegramBot.learning.SessionManager;
+import ru.rtf.telegramBot.learning.mode.MatchLearning;
+import ru.rtf.telegramBot.learning.mode.TypingLearning;
+
+import java.util.Collection;
+import java.util.NoSuchElementException;
+
+/**
+ * Обработчик команд относящихся к режимам обучения
+ */
+public class LearnCheckCommandHandler implements CommandHandler {
+    /**
+     * Количество параметров команды
+     * 1.тип режима обучения
+     * 2.имя колоды
+     */
+    private final int COUNT_PARAMS = 2;
+    /**
+     * Менеджер сессий пользователе
+     */
+    private final SessionManager sessionManager;
+
+    public LearnCheckCommandHandler(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Override
+    public String handle(DeckManager usersDecks, Long chatId, String[] params) {
+        String typeCheck = params[0].toLowerCase();
+        String deckName = params[1];
+        try {
+            Collection<Card> cardsToLearn = usersDecks.getDeck(deckName).getCards();
+
+            LearningSession learningSession;
+            switch (typeCheck) {
+                case "match":
+                    learningSession = new MatchLearning(cardsToLearn);
+                    break;
+                case "typing":
+                    learningSession = new TypingLearning(cardsToLearn);
+                    break;
+                default:
+                    return MESSAGE_COMMAND_ERROR.formatted("Неизвестный режим обучения");
+            }
+
+            return sessionManager.start(chatId, learningSession);
+
+        } catch (IllegalArgumentException | NoSuchElementException | IllegalStateException exception) {
+            return MESSAGE_COMMAND_ERROR.formatted(exception.getMessage());
+        }
+    }
+
+    @Override
+    public int getParamsCount() {
+        return COUNT_PARAMS;
+    }
+}
---
+++ src/test/java/ru/rtf/telegramBot/learning/mode/TypingLearningTest.java
@@ -0,0 +1,71 @@
+package ru.rtf.telegramBot.learning.mode;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import ru.rtf.Card;
+
+import java.util.List;
+
+/**
+ * Тестирование режима обучения "ввод термина"
+ */
+class TypingLearningTest {
+    /**
+     * Карта для обучения
+     */
+    private final List<Card> card = List.of(new Card("term", "def"));
+    /**
+     * Экземпляр режима обучения
+     */
+    private TypingLearning typingLearning;
+
+    /**
+     * Создание нового экземпляра режима обучения для каждого теста
+     */
+    @BeforeEach
+    void setUp() {
+        typingLearning = new TypingLearning(card);
+    }
+
+    /**
+     * Проверка формулировки вопроса к пользователю
+     */
+    @Test
+    @DisplayName("Формулировка вопроса")
+    void testFormQuestion() {
+        String question = typingLearning.formQuestion();
+        Assertions.assertEquals("""
+                Определение - "def".
+                Введите соответствующий термин:""", question);
+    }
+
+    /**
+     * Проверка правильного ответа
+     */
+    @Test
+    @DisplayName("Правильный ответ")
+    void testCheckRightAnswer() {
+        Assertions.assertTrue(typingLearning.checkAnswer("term"));
+    }
+
+    /**
+     * Проверка неправильного ответа
+     */
+    @Test
+    @DisplayName("Неправильный ответ")
+    void testCheckWrongAnswer() {
+        Assertions.assertFalse(typingLearning.checkAnswer("notTerm"));
+    }
+
+    /**
+     * Проверка формирования текстового описания активной карты
+     */
+    @Test
+    @DisplayName("Активная карта текстом")
+    void getActiveCardDescription() {
+        String card = typingLearning.getActiveCardDescription();
+        Assertions.assertEquals("\"term\" = def", card);
+    }
+}
