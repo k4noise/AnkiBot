ab711db2dd792a624b4c5a59520078f8923548fa
k4noise <vestertp@gmail.com>
2024-11-20 17:43:20 +0500

    Добавлена поддержка статусов карт

--- src/main/java/ru/rtf/Card.java
+++ src/main/java/ru/rtf/Card.java
@@ -7,6 +7,24 @@ import java.util.Objects;
  */
 public class Card {
     /**
+     * Минимально возможное количество баллов
+     */
+    private static final int MIN_SCORE = 0;
+    /**
+     * Максимально возможное количество баллов
+     */
+    private static final int MAX_SCORE = 12;
+    /**
+     * Минимально возможное количество баллов для получения статуса "Изучена"
+     */
+    private static final int MIN_SCORE_IN_STUDIED_STATUS = 10;
+    /**
+     * Минимально возможное количество баллов для получения статуса "Частично изучена"
+     */
+    private static final int MIN_SCORE_IN_PARTIALLY_STUDIED_STATUS = 5;
+
+
+    /**
      * Термин - идентификатор внутри колоды
      */
     private final String term;
@@ -14,6 +32,10 @@ public class Card {
      * Определение
      */
     private String definition;
+    /**
+     * Баллы
+     */
+    private int score;
 
     /**
      * Создание карты
@@ -62,6 +84,47 @@ public class Card {
         if (newDefinition.isEmpty())
             throw new IllegalArgumentException("Определение не может быть пустым");
         definition = newDefinition;
+    }
+
+    /**
+     * Добавить балл карточке
+     */
+    public void addScore() {
+        addScore(1);
+    }
+
+    /**
+     * Добавить баллы карточке
+     *
+     * @param scoreDiff Количество баллов для добавления
+     */
+    public void addScore(int scoreDiff) {
+        if (score == MAX_SCORE) {
+            return;
+        }
+        score = Math.min(MAX_SCORE, score + scoreDiff);
+    }
+
+    /**
+     * Убавить балл карточки
+     */
+    public void subtractScore() {
+        if (score == MIN_SCORE) {
+            return;
+        }
+        score--;
+    }
+
+    /**
+     * Вернуть статус карты
+     */
+    public CardLearningStatus getStatus() {
+        if (score >= MIN_SCORE_IN_STUDIED_STATUS) {
+            return CardLearningStatus.STUDIED;
+        } else if (score >= MIN_SCORE_IN_PARTIALLY_STUDIED_STATUS) {
+            return CardLearningStatus.PARTIALLY_STUDIED;
+        }
+        return CardLearningStatus.NOT_STUDIED;
     }
 
     @Override
---
+++ src/main/java/ru/rtf/CardLearningStatus.java
@@ -0,0 +1,19 @@
+package ru.rtf;
+
+/**
+ * Статус карточки
+ */
+public enum CardLearningStatus {
+    /**
+     * Изучена
+     */
+    STUDIED,
+    /**
+     * Частично изучена
+     */
+    PARTIALLY_STUDIED,
+    /**
+     * Не изучена
+     */
+    NOT_STUDIED
+}
