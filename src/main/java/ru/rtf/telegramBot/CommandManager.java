00e7a03077bfa79d3d5dd7480cbb07845f0b870a
k4noise <vestertp@gmail.com>
2024-11-26 16:19:26 +0500

    Убрана жесткая привязка `CommandManager` к списку команд

--- src/main/java/ru/rtf/telegramBot/CommandManager.java
+++ src/main/java/ru/rtf/telegramBot/CommandManager.java
@@ -22,31 +22,23 @@ public class CommandManager {
     private final Map<Long, DeckManager> userDeckManagers;
 
     /**
-     * Инициализирует поля и добавляет команды в список
+     * Инициализирует поля и добавляет команды по умолчанию в список
      *
      * @param sessionManager Менеджер сессий пользователей
      */
     public CommandManager(SessionManager sessionManager) {
-        this.userDeckManagers = new HashMap<>();
-        this.commands = new HashMap<>();
+        this(new HashMap<>());
+        createDefaultCommandsWithHandlers(sessionManager);
+    }
 
-        commands.put("/start", new StartCommandHandler());
-        commands.put("/help", new HelpCommandHandler());
-        // команды для работы с колодами
-        commands.put("/list_decks", new ListDecksCommandHandler());
-        commands.put("/create_deck", new CreateDeckCommandHandler());
-        commands.put("/rename_deck", new RenameDeckCommandHandler());
-        commands.put("/delete_deck", new DeleteDeckCommandHandler());
-        // команды для работы с картами
-        commands.put("/list_cards", new ListCardsCommandsHandler());
-        commands.put("/create_card", new CreateCardCommandHandler());
-        commands.put("/edit_card_term", new EditCardTermCommandHandler());
-        commands.put("/edit_card_def", new EditCardDefCommandHandler());
-        commands.put("/delete_card", new DeleteCardCommandHandler());
-        commands.put("/list_card", new ListCardCommandHandler());
-        //команды режимов обучения
-        commands.put("/check", new LearnCheckCommandHandler(sessionManager));
-        commands.put("/end_check", new EndCheckCommandHandler(sessionManager));
+    /**
+     * Инициализирует поля и добавляет пользовательские команды в список
+     *
+     * @param commands Хранилище команд
+     */
+    public CommandManager(Map<String, CommandHandler> commands) {
+        this.commands = commands;
+        this.userDeckManagers = new HashMap<>();
     }
 
     /**
@@ -88,5 +80,30 @@ public class CommandManager {
         }
 
         return commandParams.length >= correctCountParams;
+    }
+
+    /**
+     * Создает команды по умолчанию, включая обработчиков команд
+     *
+     * @param sessionManager Менеджер сессий пользователя
+     */
+    private void createDefaultCommandsWithHandlers(SessionManager sessionManager) {
+        commands.put("/start", new StartCommandHandler());
+        commands.put("/help", new HelpCommandHandler());
+        // команды для работы с колодами
+        commands.put("/list_decks", new ListDecksCommandHandler());
+        commands.put("/create_deck", new CreateDeckCommandHandler());
+        commands.put("/rename_deck", new RenameDeckCommandHandler());
+        commands.put("/delete_deck", new DeleteDeckCommandHandler());
+        // команды для работы с картами
+        commands.put("/list_cards", new ListCardsCommandsHandler());
+        commands.put("/create_card", new CreateCardCommandHandler());
+        commands.put("/edit_card_term", new EditCardTermCommandHandler());
+        commands.put("/edit_card_def", new EditCardDefCommandHandler());
+        commands.put("/delete_card", new DeleteCardCommandHandler());
+        commands.put("/list_card", new ListCardCommandHandler());
+        //команды режимов обучения
+        commands.put("/check", new LearnCheckCommandHandler(sessionManager));
+        commands.put("/end_check", new EndCheckCommandHandler(sessionManager));
     }
 }
