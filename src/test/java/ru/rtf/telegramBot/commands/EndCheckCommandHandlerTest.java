4b47118fcfd62385c3c7cf79402573d8a142e4b2
k4noise <vestertp@gmail.com>
2024-11-30 18:15:07 +0500

    Добавлены потерянные тесты

---
+++ src/test/java/ru/rtf/telegramBot/commands/EndCheckCommandHandlerTest.java
@@ -0,0 +1,67 @@
+package ru.rtf.telegramBot.commands;
+
+import org.junit.jupiter.api.*;
+import ru.rtf.Card;
+import ru.rtf.DeckManager;
+import ru.rtf.telegramBot.learning.SessionManager;
+import ru.rtf.telegramBot.learning.mode.MatchLearning;
+
+import java.util.Collection;
+
+/**
+ * Тест обработчика команды завершения сессии обучения
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class EndCheckCommandHandlerTest {
+    /**
+     * Обработчик команды для завершения режима обучения
+     */
+    private EndCheckCommandHandler endCheckCommandHandler;
+    /**
+     * Менеджер сессий пользователей
+     */
+    private SessionManager sessionManager;
+    /**
+     * Менеджер колод пользователя
+     */
+    private DeckManager deckManager;
+    /**
+     * Идентификатор чата
+     */
+    private final Long chatId = 1L;
+
+    @BeforeAll
+    void initHandler() {
+        deckManager = new DeckManager();
+        deckManager.addDeck("Deck");
+        deckManager.getDeck("Deck")
+                .addCard(new Card("term", "def"));
+
+        sessionManager = new SessionManager();
+        endCheckCommandHandler = new EndCheckCommandHandler(sessionManager);
+    }
+
+    /**
+     * Проверка невозможности окончания несуществующей сессии
+     */
+    @Test
+    @DisplayName("Выход вне сессии")
+    void testEndNonExistSession() {
+        String message = endCheckCommandHandler.handle(deckManager, chatId, null);
+        Assertions.assertEquals("""
+                Ошибка выполнения команды Подробности:
+                Нет активной сессии обучения""", message);
+    }
+
+    /**
+     * Проверка корректного выхода из активной сессии
+     */
+    @Test
+    @DisplayName("Выход из активной сессии")
+    void testEndExistSession() {
+        Collection<Card> cards = deckManager.getDeck("Deck").getCards();
+        sessionManager.start(chatId, new MatchLearning(cards));
+        String message = endCheckCommandHandler.handle(deckManager, chatId, null);
+        Assertions.assertEquals("Вы досрочно завершили сессию", message);
+    }
+}
---
+++ src/test/java/ru/rtf/telegramBot/commands/LearnCheckCommandHandlerTest.java
@@ -0,0 +1,109 @@
+package ru.rtf.telegramBot.commands;
+
+import org.junit.jupiter.api.*;
+import ru.rtf.Card;
+import ru.rtf.DeckManager;
+import ru.rtf.telegramBot.learning.SessionManager;
+
+/**
+ * Тест обработчика команды начала сессии обучения
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class LearnCheckCommandHandlerTest {
+    /**
+     * Идентификатор чата
+     */
+    private final Long chatId = 1L;
+    /**
+     * Обработчик команды для начала режима обучения
+     */
+    private LearnCheckCommandHandler learnCheckCommandHandler;
+    /**
+     * Менеджер сессий пользователей
+     */
+    private SessionManager sessionManager;
+    /**
+     * Менеджер колод пользователя
+     */
+    private DeckManager deckManager;
+
+    @BeforeAll
+    void setUp() {
+        deckManager = new DeckManager();
+        deckManager.addDeck("Deck");
+        deckManager.getDeck("Deck")
+                .addCard(new Card("term", "def"));
+
+        sessionManager = new SessionManager();
+        learnCheckCommandHandler = new LearnCheckCommandHandler(sessionManager);
+    }
+
+    @BeforeEach
+    void endActiveSession() {
+        if (sessionManager.hasActive(chatId)) {
+            sessionManager.end(chatId);
+        }
+    }
+
+    /**
+     * Проверка успешности запуска режима "соответствие"
+     */
+    @Test
+    @DisplayName("Запуск режима \"соответствие\"")
+    void testStartMatchLearningCheck() {
+        String message = learnCheckCommandHandler.handle(deckManager, chatId, new String[]{"match", "Deck"});
+        Assertions.assertEquals("""
+                Вы начали обучение в режиме соответствия
+                Показывается термин и определение, ваша задача - определить, соответствует ли термин определению
+                Чтобы досрочно завершить сеанс, воспользуйтесь командой /end_check
+                
+                Ваш первый вопрос: Утверждение:
+                term - def
+                1 - верно, 0 - неверно""", message);
+    }
+
+    /**
+     * Проверка успешности запуска режима "ввод"
+     */
+    @Test
+    @DisplayName("Запуск режима \"ввод\"")
+    void testStartTypingLearningCheck() {
+        String message = learnCheckCommandHandler.handle(deckManager, chatId, new String[]{"typing", "Deck"});
+        Assertions.assertEquals("""
+                Вы начали обучение в режиме ввода термина
+                Дано определение, ваша задача написать соответсвующий термин (регистр не учитывается)
+                Чтобы досрочно завершить сеанс, воспользуйтесь командой /end_check
+                
+                Ваш первый вопрос: Определение - "def"
+                Введите соответствующий термин:""", message);
+    }
+
+    /**
+     * Проверка успешности запуска режима оценки запоминания
+     */
+    @Test
+    @DisplayName("Запуск режима оценки запоминания")
+    void testStartMemoryLearningCheck() {
+        String message = learnCheckCommandHandler.handle(deckManager, chatId, new String[]{"memory", "Deck"});
+        Assertions.assertEquals("""
+                Вы начали обучение в режиме карточки
+                Вам показывается термин, ваша задача - вспомнить определение
+                После оценить, насколько хорошо вы помните определение
+                0 - не помню  1 - частично помню  2 - помню
+                Чтобы досрочно завершить сеанс, воспользуйтесь командой /end_check
+                
+                Ваш первый вопрос: Термин - "term\"""", message);
+    }
+
+    /**
+     * Проверка запуска несуществующего режима
+     */
+    @Test
+    @DisplayName("Запуск несуществующего режима")
+    void testStartNonExistCheck() {
+        String message = learnCheckCommandHandler.handle(deckManager, chatId, new String[]{"wrong", "Deck"});
+        Assertions.assertEquals("""
+                Ошибка выполнения команды Подробности:
+                Неизвестный режим обучения""", message);
+    }
+}
